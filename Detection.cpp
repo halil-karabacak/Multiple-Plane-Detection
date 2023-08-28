845bcac785c74f6ab40f9dcb7d51acec2cf25368
Halil Karabacak <halil.karabacak@kivitechnologies.com>
2023-08-28 14:48:11 +0300

    Tests completed & Algorithm fixed

--- Detection.cpp
+++ Detection.cpp
@@ -5,45 +5,47 @@ std::tuple<Eigen::Vector4d, std::vector<size_t>> PlaneSegmentation::PlaneRegress
 	return pcd.SegmentPlane(threshold, init_n, iter);
 }
 
-std::vector<Plane> PlaneSegmentation::DetectMultiPlanes(open3d::geometry::PointCloud& pcd, float min_ratio, float threshold, int iterations)
+std::vector<Plane> PlaneSegmentation::DetectMultiPlanes(const open3d::geometry::PointCloud& pcd, float min_ratio, float threshold, int iterations, int upper_limit)
 {
 	std::vector<Plane> plane_list;
 	int size = (int)pcd.points_.size();
 	auto target = pcd;
 	int count = 0;
+	int plane_count = 0;
 
-
-	while (count < (1 - min_ratio) * size) {
+	while (count < (1 - min_ratio) * size && plane_count < upper_limit) {
 		std::tuple<Eigen::Vector4d, std::vector<size_t>> plane = PlaneRegression(target, threshold=threshold, 3, iterations);
 		Plane planeObj;
 		Eigen::Vector4d plane_eq = std::get<0>(plane);
 		std::vector<size_t> index = std::get<1>(plane);
 
-		planeObj.equation = plane_eq;
-		planeObj.point_index = index;
-		
 		count += (int)index.size();
-
+		plane_count++;
 		auto res = RemoveGivenPointSet(target, index);
-		target = res.first;
-		planeObj.center = res.second;
 
+		target = res.first;
+		planeObj.center = res.second.first;
+		planeObj.point_coord = res.second.second;
+		planeObj.equation = plane_eq;
 		plane_list.push_back(planeObj);
 	}
+
 	return plane_list;
 }
 
 
-std::pair<open3d::geometry::PointCloud, Eigen::Vector3d> PlaneSegmentation::RemoveGivenPointSet(open3d::geometry::PointCloud& pcd, std::vector<size_t> index_set)
+std::pair<open3d::geometry::PointCloud, std::pair<Eigen::Vector3d, std::vector<Eigen::Vector3d>>> PlaneSegmentation::RemoveGivenPointSet(const open3d::geometry::PointCloud& pcd, std::vector<size_t> index_set)
 {
 	std::vector<Eigen::Vector3d> tmp_points; tmp_points.reserve(pcd.points_.size() - index_set.size());
 	std::vector<Eigen::Vector3d> tmp_normals; tmp_normals.reserve(pcd.points_.size() - index_set.size());
 	std::vector<Eigen::Vector3d> tmp_colors; tmp_colors.reserve(pcd.points_.size() - index_set.size());
 	std::vector<Eigen::Matrix3d> tmp_covariances; tmp_covariances.reserve(pcd.points_.size() - index_set.size());
+	open3d::geometry::PointCloud out;
 
+	std::vector<Eigen::Vector3d> plane_coord;
 	if (index_set.size() == 0) {
 		std::cout << "Set shouldn't be empty!" << std::endl;
-		return std::make_pair(pcd, Eigen::Vector3d::Identity());
+		return std::make_pair(pcd, std::make_pair(Eigen::Vector3d::Identity(), std::vector<Eigen::Vector3d>()));
 	}
 
 	double avg_x = 0, avg_y = 0, avg_z = 0;
@@ -60,16 +62,18 @@ std::pair<open3d::geometry::PointCloud, Eigen::Vector3d> PlaneSegmentation::Remo
 				tmp_covariances.push_back(pcd.covariances_[i]);
 		}
 		else {
+			plane_coord.push_back(pcd.points_[i]);
 			avg_x += pcd.points_[i].x();
 			avg_y += pcd.points_[i].y();
 			avg_z += pcd.points_[i].z();
 		}
 	}
 
-	pcd.points_ = tmp_points;
-	pcd.normals_ = tmp_normals;
-	pcd.colors_ = tmp_colors;
-	pcd.covariances_ = tmp_covariances;
+	out.points_ = tmp_points;
+	out.normals_ = tmp_normals;
+	out.colors_ = tmp_colors;
+	out.covariances_ = tmp_covariances;
 
-	return std::make_pair(pcd, Eigen::Vector3d(avg_x / index_set.size(), avg_y / index_set.size(), avg_z / index_set.size()));
+	return std::make_pair(out, std::make_pair(Eigen::Vector3d(avg_x / index_set.size(), avg_y / index_set.size(), avg_z / index_set.size()), plane_coord));
 }
+
--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@
 int main() {
     open3d::utility::SetVerbosityLevel(open3d::utility::VerbosityLevel::Debug); // Set verbosity level
 
-    std::string file_path = "../data/test2.ply";
+    std::string file_path = "../data/test1.ply";
     auto point_cloud = open3d::io::CreatePointCloudFromFile(file_path);
     
     // preprocessing
     open3d::geometry::PointCloud res = point_cloud->RemoveNonFinitePoints();
-    std::shared_ptr<open3d::geometry::PointCloud > pcd =  std::get<0>(res.RemoveStatisticalOutliers(50, 0.5));
+    std::shared_ptr<open3d::geometry::PointCloud > pcd =  std::get<0>(res.RemoveStatisticalOutliers(50, 1));
 
     auto start = std::chrono::high_resolution_clock::now();
+    
     auto planes = PlaneSegmentation::DetectMultiPlanes(*pcd, 0.05, 0.005, 2000);
+    
     auto end = std::chrono::high_resolution_clock::now();
 
     std::cout << planes.size() << " planes detected!" << std::endl;
@@ -23,4 +25,17 @@ int main() {
 
     std::cout << "Multi plane detection took: " << duration.count() << " seconds" << std::endl;
 
+    // Only for visualization!
+    int t = 0;
+    for (auto plane : planes) {
+        double color = (std::rand() % 255) / 255;
+        open3d::geometry::PointCloud a;
+        t++;
+        double rand = (double)(std::rand() % 255) / 255;
+        for (auto ind : plane.point_coord) {
+            a.points_.push_back(ind);
+            a.colors_.push_back(Eigen::Vector3d(rand, rand, rand));
+        }
+        open3d::io::WritePointCloudToPLY("Plane_" + std::to_string(t) + ".ply", a, open3d::io::WritePointCloudOption());
+    }
 }
